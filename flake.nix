57c31d71eb489da897db877ea5430ee30a68d626
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-06-22 00:06:04 -0700

    fix nix-eval-jobs src (#1)
    * fix nix-eval-jobs src
    
    * add ci
    
    * wip

--- flake.nix
+++ flake.nix
@@ -7,78 +7,62 @@
     url = "github:nix-ocaml/nix-overlays";
     inputs.flake-utils.follows = "flake-utils";
   };
-  inputs.nix-eval-jobs.url = "github:nix-community/nix-eval-jobs";
+  inputs.nix-eval-jobs-src.url = "github:nix-community/nix-eval-jobs?rev=d0b436132958c3e272df9f08e0cbe75e86527582";
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , nix-filter
-    , nix-eval-jobs
-    }:
+  outputs = { self, nixpkgs, flake-utils, nix-filter, nix-eval-jobs-src }:
     flake-utils.lib.eachDefaultSystem
       (system:
-      let
-        pkgs = nixpkgs.legacyPackages."${system}".extend (self: super: {
-          ocamlPackages = super.ocaml-ng.ocamlPackages_5_2;
+        let
+          pkgs = nixpkgs.legacyPackages."${system}".extend (self: super: {
+            ocamlPackages = super.ocaml-ng.ocamlPackages_5_2;
+          });
+          inherit (pkgs) lib makeWrapper ocamlPackages stdenv;
+          nix-eval-jobs = nix-eval-jobs-src.outputs.packages."${system}".default;
+          path = lib.makeBinPath [ nix-eval-jobs ];
+          # Needed for x86_64-darwin
+          buildDunePackage =
+            if stdenv.isDarwin && !stdenv.isAarch64 then
+              ocamlPackages.buildDunePackage.override
+                { stdenv = pkgs.overrideSDK stdenv "11.0"; }
+            else
+              ocamlPackages.buildDunePackage;
+        in
+        {
+          packages = {
+            default = buildDunePackage {
+              pname = "nix-ci-build";
+              version = "n/a";
+              src = with nix-filter.lib; filter {
+                root = ./.;
+                include = [
+                  "bin"
+                  "lib"
+                  "dune-project"
+                  "nix-ci-build.opam"
+                ];
+              };
+              nativeBuildInputs = [ makeWrapper ];
+              buildInputs = [ nix-eval-jobs ];
+              propagatedBuildInputs = with ocamlPackages; [
+                cmdliner
+                eio_main
+                logs
+                fmt
+                ppx_yojson_conv
+              ];
+              postInstall = ''
+                wrapProgram "$out/bin/nix-ci-build" --prefix PATH : ${path}
+              '';
+            };
+          };
+          devShells = {
+            default = pkgs.mkShell {
+              inputsFrom = [ self.packages.${system}.default ];
+              nativeBuildInputs = with pkgs.ocamlPackages; [
+                merlin
+                ocamlformat
+              ];
+            };
+          };
         });
-        inherit (pkgs)
-          lib
-          makeWrapper
-          ocamlPackages
-          stdenv
-          nixVersions;
-        path = lib.makeBinPath [
-          nix-eval-jobs.outputs.packages."${system}".default
-          nixVersions.nix_2_22
-        ];
-        # Needed for x86_64-darwin
-        buildDunePackage =
-          if stdenv.isDarwin && !stdenv.isAarch64
-          then
-            ocamlPackages.buildDunePackage.override
-              { stdenv = pkgs.overrideSDK stdenv "11.0"; }
-          else ocamlPackages.buildDunePackage;
-        nix-ci-build = buildDunePackage {
-          pname = "nix-ci-build";
-          version = "n/a";
-          src = with nix-filter.lib; filter {
-            root = ./.;
-            include = [
-              "bin"
-              "lib"
-              "dune-project"
-              "nix-ci-build.opam"
-            ];
-          };
-          nativeBuildInputs = [ makeWrapper ];
-          buildInputs = [ pkgs.nix-eval-jobs ];
-          propagatedBuildInputs = with ocamlPackages; [
-            cmdliner
-            eio_main
-            logs
-            fmt
-            ppx_yojson_conv
-          ];
-
-          postInstall = ''
-            wrapProgram "$out/bin/nix-ci-build" \
-              --prefix PATH : ${path}
-          '';
-        };
-      in
-      {
-        packages = {
-          default = nix-ci-build;
-        };
-        devShells = {
-          default = pkgs.mkShell {
-            inputsFrom = [ nix-ci-build ];
-            nativeBuildInputs = with pkgs.ocamlPackages; [
-              merlin
-              ocamlformat
-            ];
-          };
-        };
-      });
 }
