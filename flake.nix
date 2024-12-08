dc6dcacac9772605f72d330dc8f8ad5ff380d3d4
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-12-08 15:51:20 -0800

    update flakes / remove nix-filter/flake-utils

--- flake.nix
+++ flake.nix
@@ -1,23 +1,24 @@
 {
   description = "nix-ci-build flake";
 
-  inputs.nix-filter.url = "github:numtide/nix-filter";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs = {
-    url = "github:nix-ocaml/nix-overlays";
-    inputs.flake-utils.follows = "flake-utils";
-  };
+  inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
   inputs.nix-eval-jobs-src.url = "github:nix-community/nix-eval-jobs";
 
-  outputs = { self, nixpkgs, flake-utils, nix-filter, nix-eval-jobs-src }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
+  outputs = { self, nixpkgs, nix-eval-jobs-src }:
+    let
+      forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
         let
-          pkgs = nixpkgs.legacyPackages."${system}".extend (self: super: {
-            ocamlPackages = super.ocaml-ng.ocamlPackages_5_2;
+          pkgs = nixpkgs.legacyPackages.${system}.extend (self: super: {
+            ocamlPackages = super.ocaml-ng.ocamlPackages_5_3;
           });
-          inherit (pkgs) lib makeWrapper ocamlPackages stdenv;
-          nix-eval-jobs = nix-eval-jobs-src.outputs.packages."${system}".default;
+        in
+        f pkgs);
+    in
+    {
+      packages = forAllSystems (pkgs:
+        let
+          inherit (pkgs) lib system makeWrapper ocamlPackages stdenv;
+          nix-eval-jobs = nix-eval-jobs-src.outputs.packages.${system}.default;
           path = lib.makeBinPath [ nix-eval-jobs ];
           # Needed for x86_64-darwin
           buildDunePackage =
@@ -28,41 +29,40 @@
               ocamlPackages.buildDunePackage;
         in
         {
-          packages = {
-            default = buildDunePackage {
-              pname = "nix-ci-build";
-              version = "n/a";
-              src = with nix-filter.lib; filter {
-                root = ./.;
-                include = [
-                  "bin"
-                  "lib"
-                  "dune-project"
-                  "nix-ci-build.opam"
-                ];
-              };
-              nativeBuildInputs = [ makeWrapper ];
-              buildInputs = [ nix-eval-jobs ];
-              propagatedBuildInputs = with ocamlPackages; [
-                cmdliner
-                eio_main
-                logs
-                fmt
-                ppx_yojson_conv
-              ];
-              postInstall = ''
-                wrapProgram "$out/bin/nix-ci-build" --prefix PATH : ${path}
-              '';
-            };
-          };
-          devShells = {
-            default = pkgs.mkShell {
-              inputsFrom = [ self.packages.${system}.default ];
-              nativeBuildInputs = with pkgs.ocamlPackages; [
-                merlin
-                ocamlformat
+          default = buildDunePackage {
+            pname = "nix-ci-build";
+            version = "n/a";
+            src = let fs = lib.fileset; in fs.toSource {
+              root = ./.;
+              fileset = fs.unions [
+                ./bin
+                ./lib
+                ./dune-project
+                ./nix-ci-build.opam
               ];
             };
+            nativeBuildInputs = [ makeWrapper ];
+            buildInputs = [ nix-eval-jobs ];
+            propagatedBuildInputs = with ocamlPackages; [
+              cmdliner
+              eio_main
+              logs
+              fmt
+              ppx_yojson_conv
+            ];
+            postInstall = ''
+              wrapProgram "$out/bin/nix-ci-build" --prefix PATH : ${path}
+            '';
           };
         });
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${pkgs.system}.default ];
+          nativeBuildInputs = with pkgs.ocamlPackages; [
+            merlin
+            ocamlformat
+          ];
+        };
+      });
+    };
 }
